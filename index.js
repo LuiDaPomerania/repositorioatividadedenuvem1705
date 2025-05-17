9dc0209ecc2df5c4f444701f98cbbe9250233c22
Luigi Craveiro <craveiroluigi@gmail.com>
2025-05-17 04:48:11 -0300

    Adicionando dev e index js

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+app.get('/', (req, res) => {
+res.send('Olá, servidor Node.js com Express funcionando!');
+});
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+console.log(`Servidor rodando na porta ${PORT}`);
+});
+app.get('/info', (req, res) => {
+res.json({ linguagem: 'Node.js', biblioteca: 'Express' });
+});
