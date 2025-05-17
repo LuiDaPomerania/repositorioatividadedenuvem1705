394a100424d608840c4eb648d10424cf5060a347
Luigi Craveiro <craveiroluigi@gmail.com>
2025-05-17 04:50:27 -0300

    Finalizando tarefa

--- index.js
+++ index.js
@@ -10,3 +10,5 @@ console.log(`Servidor rodando na porta ${PORT}`);
 app.get('/info', (req, res) => {
 res.json({ linguagem: 'Node.js', biblioteca: 'Express' });
 });
+const moment = require('moment');
+console.log("Data atual:", moment().format('DD/MM/YYYY HH:mm:ss'));
